0ba82bb51ea102d7c72ca210a8da4e6a6360427f
CanvasCao <1174478646@qq.com>
2018-08-07 08:10:46 +0800

    add icon button

--- stories/button.js
+++ stories/button.js
@@ -15,4 +15,22 @@ storiesOf('Button')
             </div>
 
         ),
-    );
+    ).addWithInfo(
+    '图标按钮',
+    `当需要在 Button 内嵌入 Icon 时，可以设置 icon 属性，或者直接在 Button 内使用 Icon 组件。`,
+    () => (
+        <div>
+            <Button type="primary" shape="circle" icon="search" />
+            <Button type="primary" icon="search">Search</Button>
+            <Button shape="circle" icon="search" />
+            <Button icon="search">Search</Button>
+            <br />
+            <Button shape="circle" icon="search" />
+            <Button icon="search">Search</Button>
+            <Button type="dashed" shape="circle" icon="search" />
+            <Button type="dashed" icon="search">Search</Button>
+        </div>
+    ),
+)
+
+
