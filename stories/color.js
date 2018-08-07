ff1efafcb875220f5956d1cef8698377e4d79f55
CanvasCao <1174478646@qq.com>
2018-08-07 14:48:23 +0800

    refactor color

--- stories/color.js
+++ stories/color.js
@@ -1,38 +1,54 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-storiesOf("Color", module).add("Default", () => {
-    const ColorList = [
-        {
-            value: "#00ACAC",
-            title: "primary 01",
-        },
-        {
-            value: "#004b64",
-            title: "primary 02",
-        },
-        {
-            value: "#f84743",
-            title: "support 01",
-        },
-        {
-            value: "#FEC928",
-            title: "support 02",
-        },
-        {
-            value: "#00d263",
-            title: "support 03",
-        },
-        {
-            value: "#000000",
-            title: "text color",
-        },
-        {
-            value: "#f3f5f6",
-            title: "background color",
-        },
-    ]
 
+const liStyle = {
+    position: "relative",
+    width: 200,
+    height: 200,
+    marginBottom: 50,
+}
+
+const spanStyle = {
+    position: "absolute",
+    width: "100%",
+    bottom: "-50px",
+    left: 0,
+    lineHeight: "50px",
+    textAlign: "center",
+}
+
+const ColorList = [
+    {
+        value: "#00ACAC",
+        title: "primary 01",
+    },
+    {
+        value: "#004b64",
+        title: "primary 02",
+    },
+    {
+        value: "#f84743",
+        title: "support 01",
+    },
+    {
+        value: "#FEC928",
+        title: "support 02",
+    },
+    {
+        value: "#00d263",
+        title: "support 03",
+    },
+    {
+        value: "#000000",
+        title: "text color",
+    },
+    {
+        value: "#f3f5f6",
+        title: "background color",
+    },
+]
+storiesOf("Color", module).add("Default", () => {
     return (
         <ul style={{
             display: "flex",
@@ -40,23 +56,8 @@ storiesOf("Color", module).add("Default", () => {
         }}>
             {ColorList.map(({title, value}) => {
                 return (
-                    <li style={{
-                        position: "relative",
-                        width: 200,
-                        height: 200,
-                        backgroundColor: value,
-                        marginBottom: 50,
-                    }}
-                        key={title}>
-                            <span style={{
-                                position: "absolute",
-                                width: "100%",
-                                bottom: "-50px",
-                                left: 0,
-                                lineHeight: "50px",
-                                textAlign: "center",
-                            }}>{title}: <b>{value}</b>
-                            </span>
+                    <li style={{background: value, ...liStyle}} key={title}>
+                        <span style={spanStyle}>{title}: <b>{value}</b></span>
                     </li>
                 )
             })}
